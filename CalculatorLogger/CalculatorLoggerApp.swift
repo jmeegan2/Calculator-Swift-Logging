12068d34b62d026fca57abfee066cfd42ef44bbd
jmeegan2 <jmeegan8@gmail.com>
2023-04-03 19:59:14 -0400

    init

---
+++ CalculatorLogger/CalculatorLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorLoggerApp.swift
+//  CalculatorLogger
+//
+//  Created by James Meegan on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
